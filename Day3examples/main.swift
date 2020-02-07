3bbe0dd96ca24be35255b4ca2902043e2f1a3225
ikroop virk <virkikroop13@gmail.com>
2020-02-06 19:32:05 -0500

    [IKROOP] DAY 2

--- Day3examples/main.swift
+++ Day3examples/main.swift
@@ -9,4 +9,62 @@
 import Foundation
 
 print("Hello, World!")
+var str="hello world !"
+for c in str
+{
+    print(c)
+}
+str.append("G")
+print(str)
+print(str.count)
+print(str.unicodeScalars.count)
+print(str.endIndex)
+print(str.startIndex)
+str.insert("t", at: str.startIndex)
+print(str)
+str.insert("p", at: str.endIndex)
+print(str)
+var name = "welcome to lambton college, toronto"
+print(name.hasPrefix("welcome"))
+print(name.hasSuffix("toronto"))
+print(name.contains("to"))
+print(name.isEmpty)
+var s = ""
+var k = String()
+print(s.isEmpty)
+print(k.isEmpty)
+print(name.uppercased())
+print(name.index(after: name.startIndex))
+print(name.first)
+    print(name.last)
+print(name.randomElement())
+print(name.dropLast())
+let g = "g" as Character
+print(g.isASCII)
+print(g.asciiValue)
+print(<#T##items: Any...##Any#>)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
